727dd037574215d4e21f2ca4183ea7e401b73a13
Laptop <micahcorbintanner@gmail.com>
2025-07-30 18:17:48 -0600

    Added Create New Product

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -9,5 +9,9 @@ namespace Testing
         IEnumerable<Product> GetAllProducts();
         Product GetProduct(int id);
         void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+
     }
 }
